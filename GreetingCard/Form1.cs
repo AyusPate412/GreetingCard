3f22bf839118556004323f01f611c5ba714fb6de
AyusPate412 <payush44477@gmail.com>
2024-04-25 14:54:49 -0400

    newer

--- GreetingCard/Form1.cs
+++ GreetingCard/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
+using System.Media;
 
 //24 April, 2024
 //Greeting Card
@@ -16,13 +17,19 @@ namespace GreetingCard
 {
     public partial class Greetings : Form
     {
-        Pen pinkPen = new Pen(Color.Pink, 1);
-        Pen blackPen = new Pen(Color.Black, 1);
+        // creates different color pens
+        Pen whitePen = new Pen(Color.White, 1);
 
-        SolidBrush blueBrush = new SolidBrush(Color.Blue);
-        SolidBrush pinkBrush = new SolidBrush(Color.LightPink);
+        // creates a Yellow brush
+        SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
 
-        Font drawFont = new Font("Arial", 42, FontStyle.Bold);
+        // sets up the font, size and style
+        Font drawFont = new Font("Times New Roman", 42, FontStyle.Bold);
+        Font drawFont2 = new Font("Times New Roman", 20, FontStyle.Bold);
+
+        //setup a new sound
+        new SoundPlayer celebration = new SoundPlayer(Properties.Resources.dhol);
+        new SoundPlayer blackSmith = new SoundPlayer(Properties.Resources.blacksmithWorkshop);
 
         public Greetings()
         {
@@ -31,84 +38,104 @@ namespace GreetingCard
 
         private void Greetings_Shown(object sender, EventArgs e)
         {
+            
             Graphics g = this.CreateGraphics();
 
             //Set background Color
-            g.Clear(Color.LightBlue);
+            g.Clear(Color.Black);
 
             //Writes Happy Diwali
-            g.DrawString("Happy", drawFont, blueBrush, 315, 100);
-            g.DrawString("Diwali", drawFont, blueBrush, 315, 200);
+            g.DrawString("Happy", drawFont, yellowBrush, 315, 100);
+            g.DrawString("Diwali", drawFont, yellowBrush, 315, 200);
 
             //Draw Wire
-            g.DrawLine(blackPen, 0, 25, 1000, 25);
+            g.DrawLine(whitePen, 0, 25, 1000, 25);
 
            //Draw Lights   
             for (int i = 0; i < 27; i++)
             {
-                g.DrawEllipse(pinkPen, 10 + i * 30, 25, 10, 20);
-                g.FillEllipse(pinkBrush, 10 + i * 30, 25, 10, 20);
+                g.DrawEllipse(whitePen, 10 + i * 30, 25, 10, 20);
+                g.FillEllipse(yellowBrush, 10 + i * 30, 25, 10, 20);
             }
 
             //Ballons
             for (int i = 0; i < 3; i++)
             {
-                g.DrawEllipse(pinkPen, 15 + i * 50, 300, 30, 50);
-                g.FillEllipse(pinkBrush, 15 + i * 50, 300, 30, 50);
-                g.DrawLine(blackPen, 30 + i * 50, 350, 80, 400);
+                g.DrawEllipse(whitePen, 15 + i * 50, 300, 30, 50);
+                g.FillEllipse(yellowBrush, 15 + i * 50, 300, 30, 50);
+                g.DrawLine(whitePen, 30 + i * 50, 350, 80, 400);
             }
 
+            //Creates another sets of ballons
             for (int i = 0; i < 3; i++)
             {
-                g.DrawEllipse(pinkPen, 200 + i * 50, 300, 30, 50);
-                g.FillEllipse(pinkBrush, 200 + i * 50, 300, 30, 50);
-                g.DrawLine(blackPen, 200 + i * 50, 350, 280, 400);
+                g.DrawEllipse(whitePen, 200 + i * 50, 300, 30, 50);
+                g.FillEllipse(yellowBrush, 200 + i * 50, 300, 30, 50);
+                g.DrawLine(whitePen, 215 + i * 50, 350, 265, 400);
+            }
+
+            //creates another sets of ballons
+            for (int i = 0; i < 3; i++)
+            {
+                g.DrawEllipse(whitePen, 600 + i * 50, 300, 30, 50);
+                g.FillEllipse(yellowBrush, 600 + i * 50, 300, 30, 50);
+                g.DrawLine(whitePen, 615 + i * 50, 350, 665, 400);
             }
         }
 
         private void Greetings_Click(object sender, EventArgs e)
         {
             Graphics g = this.CreateGraphics();
-
+            // creates random and creates a int flash
             Random rand = new Random();
             int flash = 0;
 
-            while (flash < 2) {
+            //flashes the lights different color 5 times
+            while (flash < 5) {
                 for (int i = 0; i < 27; i++)
                 {
                     Color randomColor = Color.FromArgb(rand.Next(255), rand.Next(256), rand.Next(256));
                     g.FillEllipse(new SolidBrush(randomColor), 10 + i * 30, 25, 10, 20);
                 }
-
                 flash++;    
                 Thread.Sleep(1000);
             }
             //ballon's y value
             int y = 300;
 
-            //while loop to move the ballon up
-            while (y > 0)
+            //while loop to move the ballon up 
+            celebration.PlayLooping();
+            
+            while (y > - 250)
             {
-                g.Clear(Color.LightBlue);
+                g.Clear(Color.Black);
+
+                //moves the text and the ballons up
                 for (int i = 0; i < 3; i++)
                 {
-                    g.DrawEllipse(pinkPen, 15 + i * 50, y, 30, 50);
-                    g.FillEllipse(pinkBrush, 15 + i * 50, y, 30, 50);
-                    g.DrawLine(blackPen, 30 + i * 50, y+50, 80, y+120);
-                }
-                Thread.Sleep(10);
-                y--;
+                    g.DrawEllipse(whitePen, 15 + i * 50, y, 30, 50);
+                    g.FillEllipse(yellowBrush, 15 + i * 50, y, 30, 50);
+                    g.DrawLine(whitePen, 30 + i * 50, y + 50, 80, y + 120);
 
-                for (int i = 0; i < 3; i++)
-                {
-                    g.DrawEllipse(pinkPen, 15 + i * 50, y, 30, 50);
-                    g.FillEllipse(pinkBrush, 15 + i * 50, y, 30, 50);
-                    g.DrawLine(blackPen, 30 + i * 50, y + 50, 80, y + 120);
-                }
-                Thread.Sleep(10);
-                y--;
+                    g.DrawEllipse(whitePen, 200 + i * 50, y, 30, 50);
+                    g.FillEllipse(yellowBrush, 200 + i * 50, y, 30, 50);
+                    g.DrawLine(whitePen, 215 + i * 50, y + 50, 265, y + 120);
 
+                    g.DrawEllipse(whitePen, 600 + i * 50, y, 30, 50);
+                    g.FillEllipse(yellowBrush, 600 + i * 50, y, 30, 50);
+                    g.DrawLine(whitePen, 615 + i * 50, y + 50, 665, y + 120);
+
+                    g.DrawString("Happy", drawFont, yellowBrush, 400, y);
+                    g.DrawString("Diwali", drawFont, yellowBrush, 400, y + 100);
+
+                    g.DrawString("From -", drawFont2, yellowBrush, 400, y + 200);
+                    g.DrawString("Ayush Patel", drawFont2, yellowBrush, 400 + 100, y + 200);
+                }
+                Thread.Sleep(1);
+                y--;
             }
+            celebration.Stop();
+
         }
     }
 }
